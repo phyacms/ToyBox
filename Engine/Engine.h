83cba9efe33526941c5b57749e9e4c1802bb5bac
Myeungsang Cho <phyacms@gmail.com>
2021-11-01 16:39:07 +0900

    Introduced some standard-related features.

--- Engine/Engine.h
+++ Engine/Engine.h
@@ -12,5 +12,23 @@
 #include <memory>
 #include <stdexcept>
 
+// Common Setups
+namespace stdhelp
+{
+	template<typename... Ts>
+	class overloaded final
+		: private Ts...
+	{
+	public:
+		using Ts::operator()...;
+	};
+}
+
 // Macros
 #define USTR(x) u#x
+
+// Tweaks
+// @NOTE: https://devblogs.microsoft.com/cppblog/msvc-cpp20-and-the-std-cpp20-switch/
+#if _MSC_VER >= 1929 // VS2019 v16.10 and later
+#define no_unique_address msvc::no_unique_address
+#endif
