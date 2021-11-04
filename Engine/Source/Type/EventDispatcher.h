4e74813b3053319c2f37d9535a18f12c4a428f5b
Myeungsang Cho <phyacms@gmail.com>
2021-11-04 20:34:40 +0900

    Added event dispatcher class template.

---
+++ Engine/Source/Type/EventDispatcher.h
@@ -0,0 +1,34 @@
+﻿// Copyrights 2021 by phyacms. All Rights Reserved.
+
+#pragma once
+
+#include "Engine.h"
+#include "Type/MulticastDelegate.h"
+
+template<typename T>
+class TEventDispatchPolicy;
+
+template<typename R, typename... Ts>
+class TEventDispatchPolicy<R(Ts...)> final
+	: public IBroadcastPolicy<R(Ts...)>
+{
+private:
+	static_assert(std::is_same_v<R, bool>);
+	using typename IBroadcastPolicy<R(Ts...)>::FExecutors;
+
+public:
+	virtual R Broadcast(const FExecutors& Executors, Ts... Params) const override final
+	{
+		for (const auto& [Index, Delegate] : Executors)
+		{
+			if (Delegate.Execute(std::forward<Ts>(Params)...))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+template<typename T>
+using TEventDispatcher = TMulticastDelegate<T, TEventDispatchPolicy<T>>;
