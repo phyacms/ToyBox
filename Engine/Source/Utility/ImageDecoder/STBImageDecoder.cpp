a9fc33620781b920add5e12b49a52daee7f6fc9b
Myeungsang Cho <phyacms@gmail.com>
2021-12-14 15:40:21 +0900

    Implemented stb_image-based image decoder.

---
+++ Engine/Source/Utility/ImageDecoder/STBImageDecoder.cpp
@@ -0,0 +1,45 @@
+﻿// Copyrights 2021 by phyacms. All Rights Reserved.
+
+#include "Engine.h"
+#include "STBImageDecoder.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#undef STB_IMAGE_IMPLEMENTATION
+
+struct STBImage final
+{
+	stbi_uc* Bytes{};
+	int Width{};
+	int Height{};
+	int Channels{};
+
+	explicit STBImage(const fs::path& ImageFilePath)
+		: Bytes{}
+		, Width{}
+		, Height{}
+		, Channels{} { Bytes = ::stbi_load(ImageFilePath.generic_string().c_str(), &Width, &Height, &Channels, 4); }
+	~STBImage() noexcept { if (IsValid()) { ::stbi_image_free(Bytes); } }
+
+	inline bool IsValid() const noexcept { return Bytes != nullptr; }
+};
+
+FImageFrame FSTBImageDecoder::LoadImage(const fs::path& ImageFilePath) const noexcept
+{
+	if (!IsValid())
+	{
+		return {};
+	}
+
+	STBImage LoadedImage{ ImageFilePath };
+	if (!LoadedImage.IsValid())
+	{
+		return {};
+	}
+
+	const auto RowPitch{ static_cast<std::size_t>(LoadedImage.Width) * 4 };
+	FImageFrame Frame{
+		.RGBAs{ FBytes{ RowPitch * LoadedImage.Height } },
+		.RowPitch{ RowPitch } };
+	std::memcpy(Frame.RGBAs.GetPtr(), LoadedImage.Bytes, Frame.RGBAs.GetByteSize());
+	return Frame;
+}
---
+++ Engine/Source/Utility/ImageDecoder/STBImageDecoder.h
@@ -0,0 +1,21 @@
+﻿// Copyrights 2021 by phyacms. All Rights Reserved.
+
+#pragma once
+
+#include "IImageDecoder.h"
+
+class FSTBImageDecoder final
+	: public IImageDecoder
+{
+public:
+	FSTBImageDecoder() = default;
+	FSTBImageDecoder(const FSTBImageDecoder&) = default;
+	FSTBImageDecoder& operator=(const FSTBImageDecoder&) & = default;
+	FSTBImageDecoder(FSTBImageDecoder&&) noexcept = default;
+	FSTBImageDecoder& operator=(FSTBImageDecoder&&) & noexcept = default;
+	virtual ~FSTBImageDecoder() noexcept = default;
+
+public:
+	inline virtual bool IsValid() const noexcept override final { return true; }
+	virtual FImageFrame LoadImage(const fs::path& ImageFilePath) const noexcept override final;
+};
